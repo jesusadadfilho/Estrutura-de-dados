3751a8ba576cac6b0f121e12ede9b652d8abe2cd
jesusadadfilho <jesusadadfilho@gmail.com>
2017-11-13 23:47:46 -0300

    templates

---
+++ estudo template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+inline T const& Maior (T const& a, T const& b) { 
+   return a < b ? b:a; 
+}
+
+int main () {
+   int i = 39;
+   int j = 20;
+   cout << "Maior(i, j): " << Maior(i, j) << endl; 
+
+   double f1 = 13.5; 
+   double f2 = 20.7; 
+   cout << "Maior(f1, f2): " << Maior(f1, f2) << endl; 
+
+   string s1 = "Hello"; 
+   string s2 = "World"; 
+   cout << "Maior(s1, s2): " << Maior(s1, s2) << endl; 
+
+   return 0;
+}
